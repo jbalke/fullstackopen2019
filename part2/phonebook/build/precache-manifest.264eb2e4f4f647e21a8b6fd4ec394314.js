5ca79ec98f0b438834c70fe1bfe1d7efab8dcf15
John Balke <john.balke@gmail.com>
2019-10-10 17:19:07 +0100

    Updated for deployment to Heroku.

---
+++ part2/phonebook/build/precache-manifest.264eb2e4f4f647e21a8b6fd4ec394314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0778c1dc5374f4919d58f73a4092fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41281509d9bd8a94329a",
+    "url": "/static/css/main.efdd2a1d.chunk.css"
+  },
+  {
+    "revision": "c2017f90ecbb9f2d2dc7",
+    "url": "/static/js/2.b62e9c3e.chunk.js"
+  },
+  {
+    "revision": "41281509d9bd8a94329a",
+    "url": "/static/js/main.36f4b274.chunk.js"
+  },
+  {
+    "revision": "abfe687251a52a02837d",
+    "url": "/static/js/runtime-main.ce00a3d8.js"
+  }
+]);
